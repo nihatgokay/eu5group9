322d39717c23e78c5d065db3241ef634cf4abe6a
nihatgokay <80921651+nihatgokay@users.noreply.github.com>
2021-03-19 21:35:50 +0300

    Add files via upload
    Assignment_Day_02

---
+++ day_2/Question3.java
@@ -0,0 +1,21 @@
+package day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		System.out.println("1 F                !!");
+		System.out.println("2  U               K");
+		System.out.println("3   L             C");
+		System.out.println("4    L           A");
+		System.out.println("5     S         T");
+		System.out.println("6      T       S");
+		System.out.println("7       A     L");
+		System.out.println("8        C   L");
+		System.out.println("9         K U");
+		System.out.println("10         F");
+		
+		
+
+	}
+
+}
